4b46daa32d7b3b8e8ecb4ffc50ffde71c6c36f32
Elspeth <114708141+spethstation@users.noreply.github.com>
2025-01-12 14:31:28 -0400

    Update sim2v2.c

--- sim2v2.c
+++ sim2v2.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+#define BUFSIZE 256
 
+// This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    if (argc != 2) {
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
 
-    struct stat file_stat;
-    if (stat(argv[1], &file_stat) == 0) {
-        printf("File size: %lld bytes\n", (long long)file_stat.st_size);
-    } else {
-        perror("Error getting file information");
-        return -1;
-    }
-
+    char cmd[BUFSIZE];
+    snprintf(cmd, BUFSIZE, "wc -c < %s", argv[1]);
+    system(cmd);
     return 0;
 }
+
