d86513a863ac4ec923c59715acb390ac4b832e6e
Elspeth <114708141+spethstation@users.noreply.github.com>
2025-01-12 14:53:50 -0400

    Update sim2v2.c

--- sim2v2.c
+++ sim2v2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- 
+
 #define BUFSIZE 256
 
 // This program prints the size of a specified file in bytes
@@ -12,9 +12,23 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    char cmd[BUFSIZE];
-    snprintf(cmd, BUFSIZE, "wc -c < %s", argv[1]);
-    system(cmd);
+    FILE* file = fopen(argv[1], "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fclose(file);
+
+    if (file_size < 0) {
+        fprintf(stderr, "Error determining file size.\n");
+        return -1;
+    }
+
+    printf("File size: %ld bytes\n", file_size);
+
     return 0;
 }
 
